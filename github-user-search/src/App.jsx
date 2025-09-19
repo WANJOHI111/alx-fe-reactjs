d059829808d1c6a59cbec97b8ae9064ea9d94ba1
WANJOHI111 <brianwanjohi87@gmail.com>
2025-09-19 16:53:51 +0300

    Set up GitHub User Search project with initial structure

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>GitHub User Search</h1>
+      <p>Start building your app here...</p>
+    </div>
+  );
+}
+
+export default App;
