8f76c0960aa43547b559ef2784dd95181e6b60a3
WANJOHI111 <brianwanjohi87@gmail.com>
2025-09-19 17:03:58 +0300

    Set up GitHub User Search project with initial structure

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Placeholder from "./components/Placeholder";
 
 function App() {
   return (
     <div style={{ textAlign: "center", padding: "2rem" }}>
       <h1>GitHub User Search</h1>
       <p>Start building your app here...</p>
+      <Placeholder />
     </div>
   );
 }
---
+++ github-user-search/src/components/Placeholder.jsx
@@ -0,0 +1,5 @@
+function Placeholder() {
+  return <p>This is a placeholder component. Replace me later!</p>;
+}
+
+export default Placeholder;
